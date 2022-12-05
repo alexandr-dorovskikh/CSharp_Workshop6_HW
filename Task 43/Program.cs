f06b5fe9d3c060f5c8a6a0d8e7fcc794033f9301
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-05 21:36:34 +0700

    Task 43 added

---
+++ Task 43/Program.cs
@@ -0,0 +1,16 @@
+﻿double GetVar(string VarName)
+{
+    Console.Write($"{VarName} = ");
+    return Convert.ToDouble(Console.ReadLine());    
+}
+
+string GetIntersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    return $"({x}; {y})";
+
+}
+
+Console.WriteLine(GetIntersection(GetVar("b1"), GetVar("k1"), GetVar("b2"), GetVar("k2")));
