01e37f5121b14a0c734ec30f47ab9c92197aeb4e
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-05 21:03:13 +0700

    Task 41 added

---
+++ Task 41/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GetNumsArray()
+{
+    Console.WriteLine("Enter the numbers separated by ',':");
+
+    string NumsString = Console.ReadLine() ?? string.Empty;
+
+    return NumsString.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+}
+
+int CountOfPositiveNums(int[] NumsArray)
+{
+    int Result = 0;
+    foreach(int num in NumsArray)
+    {
+        if (num > 0) Result++;
+    }
+    return Result;
+}
+
+int[] NumsArray = GetNumsArray();
+Console.WriteLine($"Count of positive nums: {CountOfPositiveNums(NumsArray)}");
