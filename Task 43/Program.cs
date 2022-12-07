d30415138aca42aef360d1d9004f0687559c3422
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-07 20:13:22 +0700

    Сhecks for matching and parallel lines have been added

--- Task 43/Program.cs
+++ Task 43/Program.cs
@@ -6,11 +6,18 @@
 
 string GetIntersection(double b1, double k1, double b2, double k2)
 {
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k1 * x + b1;
+    
+    if (b1 == b2 && k1 == k2)
+        return $"The lines match";
+    if (k1 == k2)
+        return $"The lines are parallel";
+    else
+    {    
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
 
-    return $"({x}; {y})";
-
+        return $"({x}; {y})";
+    }
 }
 
 Console.WriteLine(GetIntersection(GetVar("b1"), GetVar("k1"), GetVar("b2"), GetVar("k2")));
